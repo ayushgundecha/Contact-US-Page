0e19f0d4d0cd430340e5452d52f4b34dfbf8912d
AYUSH GUNDECHA <96761727+ayushgundecha@users.noreply.github.com>
2023-02-09 10:02:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        skyblue: '#00BFFF',
+      }
+    },
+  },
+  plugins: [],
+}
